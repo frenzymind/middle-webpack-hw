f6889b3f482613c0de9e11c700ea5705750c76e0
Nikolay <k@uilim.ru>
2024-12-26 22:25:51 +0800

    Build: Remove devtool from production build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -5,7 +5,6 @@ const commonConfig = require('./webpack.config.common')
 
 module.exports = merge(commonConfig, {
   mode: 'production',
-  devtool: 'source-map',
   optimization: {
     minimize: true,
     minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
